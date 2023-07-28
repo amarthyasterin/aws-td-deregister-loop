f9f7fe89acb0e133665e9f29169c329a7976a102
Amarthya Sterin <amarthyasterin@gmail.com>
2023-07-28 19:52:25 +0530

    Create td-deregister-loop.sh

---
+++ td-deregister-loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+TASKNAME="dev-mta-taxhero-co-uk-TD"
+START=1
+END=387
+
+for y in $(seq $START $END)
+do
+    aws ecs deregister-task-definition --task-definition $TASKNAME:$y --no-cli-pager
+    sleep 5
+    echo "The task $TASKNAME:$y has been deleted"
+done
+~               
